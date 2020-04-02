c16d7b372830444ea6adba9fc639a93385512eda
Matthew Maclean <matthew.maclean@twosigma.com>
2020-04-02 17:28:58 +0000

    pulling Matthew's PR to dev

--- gen-graphs.sh
+++ gen-graphs.sh
@@ -4,6 +4,10 @@ python3 src/stationgraph/get_equipment_list.py \
 python3 src/stationgraph/station_to_elevator.py \
   --master-list data/raw/EE_master_list.csv \
  > data/processed/stationgraph/elevator_to_line_dir_station.csv
+python3 src/stationgraph/station_to_station.py \
+  --routes data/raw/google_transit/routes.txt \
+  --stop-times data/raw/google_transit/stop_times.txt \
+ > data/processed/stationgraph/station_to_station.csv
 python3 src/stationgraph/buildgraphs.py \
   --no-inaccessible \
   --no-escalators \
