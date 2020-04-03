dffeaf3090280450b3952e04ed7e34b2982cd10c
Kaushik Mohan <kmohan@twosigma.com>
2020-04-03 18:43:22 +0000

    updated figures and script to include node colors

--- src/visualization/visualize_graphs.R
+++ src/visualization/visualize_graphs.R
@@ -16,6 +16,21 @@ option_list = list(
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 
+mapcolors <- function(x){
+    if(x=="Elevator"){
+        return("lightblue")
+    }else if(x=='Street'){
+        return("orange")
+    }else if(x=="Platform"){
+        return("white")
+    }else if(grepl(pattern = "Mezzanine",x = x)){
+        return("yellow")
+    }else if(x=="Train"){
+        return("red")
+    }else {
+        return("black")
+    }
+}
 
 g <- read_graph(opt$graph,format = 'graphml')
 routes <- read.csv(opt$routes,stringsAsFactors = F)
@@ -24,6 +39,8 @@ ee <- read.csv(opt$elevators,stringsAsFactors = F)
 comp = components(g)
 V(g)$component <- comp$membership
 
+cols <- sapply(V(g)$node_type,FUN=mapcolors,simplify = TRUE)
+V(g)$color <- cols
 V(g)$color[V(g)$node_type=="Train"] <- paste0("#",routes$route_color[match(gsub("-.*","",V(g)$label[V(g)$node_type=="Train"]),routes$route_id)])
 V(g)$color[V(g)$color == "#NA" | V(g)$color == "#"] <- 'grey'
 
@@ -43,7 +60,7 @@ for(x in unique_stations){
     par(mfrow=c(length(comps),1))
     for(i in comps){
         h <- induced_subgraph(g,V(g)[V(g)$component == i])
-            plot(h,vertex.size=8,vertex.label.degree=67.5,vertex.shape=ifelse(V(h)$node_type=='Train','square','circle'),
+            plot(h,vertex.size=8,vertex.label.degree=67.5,vertex.shape=ifelse(V(h)$node_type=='Train','square','circle'),color=V(h)$color,
                  vertex.label=gsub("-","\n",V(h)$label),vertex.label.cex=1.75,vertex.label.color='black',vertex.label.dist=-1.5,
                  layout=layout_as_tree(h,root=V(h)[V(h)$node_type == "Street"]),main=unique(ee$station_name[ee$equipment_id %in% V(h)$label]))
     }
