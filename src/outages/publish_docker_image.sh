fc40b6b4ca0b086c0dfc12be2361bd27c06bb7b6
Kaushik Mohan <kaushik.s.mohan@gmail.com>
2020-10-15 15:08:01 -0400

    pushing latest publish script

--- src/outages/publish_docker_image.sh
+++ src/outages/publish_docker_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build . -t mta_outage_collector
-docker tag mta_outage_collector gcr.io/ts-dataclinic-dev/mta_outage_collector:tag2
-docker push gcr.io/ts-dataclinic-dev/mta_outage_collector:tag2
+docker tag mta_outage_collector gcr.io/ts-dataclinic-dev/mta_outage_collector:tag3
+docker push gcr.io/ts-dataclinic-dev/mta_outage_collector:tag3
